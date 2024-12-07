72e50cd2d67d97270bb5a8e616bae03b556d4b37
MichaelBalcerzak <balcerzak.michael@gmail.com>
2024-12-07 15:47:42 -0500

    updade

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
+  background-color: #02d9ff;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
